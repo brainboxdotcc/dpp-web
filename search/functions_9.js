8abf389c3c0252e9fd20922ed69c7628b999af35
brain <craigedwards@brainbox.cc>
2021-10-05 17:00:18 +0100

    automatic commit

--- search/functions_9.js
+++ search/functions_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['load_5fimage_0',['load_image',['../classdpp_1_1emoji.html#a33694c119238d58da105c55fdf9dbcd9',1,'dpp::emoji::load_image()'],['../classdpp_1_1role.html#a6d46d3e4a1b8e166f4a26da58bf119dc',1,'dpp::role::load_image()'],['../classdpp_1_1webhook.html#a1ce7edac375941837b9f2e8279ea7443',1,'dpp::webhook::load_image()']]],
+  ['load_5fimage_0',['load_image',['../classdpp_1_1emoji.html#a33694c119238d58da105c55fdf9dbcd9',1,'dpp::emoji::load_image()'],['../classdpp_1_1role.html#a6d46d3e4a1b8e166f4a26da58bf119dc',1,'dpp::role::load_image()'],['../classdpp_1_1webhook.html#ac170fa1d18eacf094fea2b8ad6c96e9b',1,'dpp::webhook::load_image()']]],
   ['localtime_5fr_1',['localtime_r',['../namespacedetail.html#af0a05177ff15d17183395170cd81a3a0',1,'detail']]],
   ['localtime_5fs_2',['localtime_s',['../namespacedetail.html#acca4638afe6cbc4e38e3901cd0c16186',1,'detail']]],
   ['log_3',['log',['../classdpp_1_1cluster.html#a13689440c9cc80279d415c563a6363c0',1,'dpp::cluster::log()'],['../classdpp_1_1discord__client.html#a9880e3f13c00cd6e4397fe64ae23b4d0',1,'dpp::discord_client::log()'],['../classdpp_1_1discord__voice__client.html#a75e6ecd47216794a447f2b1def837852',1,'dpp::discord_voice_client::log()'],['../classdpp_1_1ssl__client.html#a6037b1a66cf83e022655bd47f0df61a5',1,'dpp::ssl_client::log()']]],
