92cda7a638dd94bada80dd8dfbf9ebde7b9bd462
brain <craigedwards@brainbox.cc>
2021-10-08 16:02:49 +0100

    automatic commit

--- 9.0.6/md_docpages_03_example_programs.js
+++ 9.0.6/md_docpages_03_example_programs.js
@@ -3,10 +3,12 @@ var md_docpages_03_example_programs =
     [ "Creating Your First Bot", "firstbot.html", null ],
     [ "Using Slash Commands and Interactions", "slashcommands.html", null ],
     [ "Creating a Sound Board", "soundboard.html", null ],
+    [ "Streaming Ogg Opus file", "oggopus.html", null ],
     [ "Join or switch to the voice channel of the user issuing a command", "joinvc.html", null ],
     [ "Integrating with spdlog", "spdlog.html", null ],
     [ "Using component interactions (buttons)", "components.html", null ],
     [ "Using component interactions (select menus)", "components3.html", null ],
     [ "Using component interactions (advanced)", "components2.html", null ],
-    [ "Using a command handler object", "commandhandler.html", null ]
+    [ "Using a command handler object", "commandhandler.html", null ],
+    [ "Using sub-commands in slash commands", "subcommands.html", null ]
 ];
--- 9.0.7/classdpp_1_1http__request.js
+++ 9.0.7/classdpp_1_1http__request.js
@@ -1,14 +1,17 @@
 var classdpp_1_1http__request =
 [
     [ "http_request", "classdpp_1_1http__request.html#a46242b648249b5e8a67e2abdb306c3b3", null ],
+    [ "http_request", "classdpp_1_1http__request.html#a7a2134b1a466d6f523a37c5631376073", null ],
     [ "~http_request", "classdpp_1_1http__request.html#a94d82f10c9922b400b1dd5b8459a6691", null ],
     [ "complete", "classdpp_1_1http__request.html#a6f350d4340fe77e224783091607f63eb", null ],
     [ "is_completed", "classdpp_1_1http__request.html#ab5f1688d8b657f6d0f0a4dd8df7b8731", null ],
-    [ "Run", "classdpp_1_1http__request.html#a620ba104acf6e2c25bcc844857fd44d3", null ],
+    [ "Run", "classdpp_1_1http__request.html#a49af6bb0b85e162b79162b701545cbaa", null ],
     [ "endpoint", "classdpp_1_1http__request.html#a31cf6b360a893292435ea56b8d6a94c6", null ],
     [ "file_content", "classdpp_1_1http__request.html#adcff8fd11769c1b04854cc56137c356b", null ],
     [ "file_name", "classdpp_1_1http__request.html#a3e56eb82e308ec41c0b15f70501a3d93", null ],
     [ "method", "classdpp_1_1http__request.html#a6717d11da20292e2dc77826def3aaef3", null ],
+    [ "mimetype", "classdpp_1_1http__request.html#aedac6464c95174507a322861191ccc02", null ],
     [ "parameters", "classdpp_1_1http__request.html#a349ce3f41c300aeac915d38f8f1d8642", null ],
-    [ "postdata", "classdpp_1_1http__request.html#adab1976475788ca96b5b52349367a322", null ]
+    [ "postdata", "classdpp_1_1http__request.html#adab1976475788ca96b5b52349367a322", null ],
+    [ "req_headers", "classdpp_1_1http__request.html#a7248f6e47440f670e2a9adbfb50022bc", null ]
 ];
