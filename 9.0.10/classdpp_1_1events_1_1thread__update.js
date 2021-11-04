f3114154ba4290d4fd32c4cb356ac7e4e8677494
brain <craigedwards@brainbox.cc>
2021-11-04 17:43:50 +0000

    automatic commit

---
+++ 9.0.10/classdpp_1_1events_1_1thread__update.js
@@ -0,0 +1,4 @@
+var classdpp_1_1events_1_1thread__update =
+[
+    [ "handle", "classdpp_1_1events_1_1thread__update.html#aba954310cadc1eacbaca2978e9dac2bb", null ]
+];
---
+++ 9.0.10/navtreedata.js
@@ -0,0 +1,88 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "D++ (DPP)", "index.html", [
+    [ "D++ Developer Documentation", "index.html", "index" ],
+    [ "Building D++", "md_docpages_02_build.html", "md_docpages_02_build" ],
+    [ "Creating a Discord Bot", "md_docpages_02_building_a_bot.html", "md_docpages_02_building_a_bot" ],
+    [ "Example Programs", "md_docpages_03_example_programs.html", "md_docpages_03_example_programs" ],
+    [ "Advanced Reference", "md_docpages_04_advanced_reference.html", "md_docpages_04_advanced_reference" ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", "namespacemembers_eval" ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classdpp_1_1cluster.html#a056e4191c03dd210c2a410b6599a0bf9",
+"classdpp_1_1cluster.html#a62516361826a0f9256d2ff79b93b9f86",
+"classdpp_1_1cluster.html#ad66ab98a72eb1ba8d2686fcaa1eb49db",
+"classdpp_1_1discord__client.html#a53cda56d5d61e3371bd7be2424e7ff07",
+"classdpp_1_1dispatcher.html#a1588d00ae704ab4b14eff4e596071b78",
+"classdpp_1_1events_1_1guild__member__add.html#aae35805b0315d3febf7fd318320a7497",
+"classdpp_1_1guild.html#a8e13e2dd21e91104a1e8335990046bf7",
+"classdpp_1_1invite.html#aedc01d96281305a15b3d23a994927a5c",
+"classdpp_1_1ssl__client.html#a8022a8002cfc6f394016546168ef4085",
+"classdpp_1_1websocket__client.html#a809b342e343bd370141ba25a5e90cf64",
+"namespacedpp.html#a022dce6b086ec4515384eaece59c84c3",
+"namespacedpp.html#a7e036e9909741e00af34e2505117218da0c44c916411b26b1b0a3b5288c346a41",
+"namespacedpp.html#ae6a96c54dc4f6dad8f8f6bbe05fea254",
+"structdetail_1_1chrono__formatter.html#af671b4ebeb476af2255cae8213504c1d",
+"structdpp_1_1channel__pins__update__t.html#a5afbcd36109734ad0fca14ec848b73b7",
+"structdpp_1_1embed__footer.html#aba53ba63861ea7dc412973c274923b6d",
+"structdpp_1_1guild__role__create__t.html#a2f07681bfc59700b451764013a9d4e13",
+"structdpp_1_1message.html",
+"structdpp_1_1message__reaction__remove__emoji__t.html#aeecf3d9382565bc7f025062df0fa54a2",
+"structdpp_1_1stage__instance__create__t.html#a11d50c4d4bdc7e078e095ec598684014",
+"structdpp_1_1user__update__t.html",
+"structformatter_3_01std_1_1tm_00_01_char_01_4.html#a57c0d36c7943de87688eeae9c3bd0b28"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
