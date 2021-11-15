5debbbe41efddbbb85d655c05295704d2985f995
brain <craigedwards@brainbox.cc>
2021-11-15 18:06:20 +0000

    automatic commit

---
+++ structdpp_1_1guild__scheduled__event__user__remove__t.js
@@ -0,0 +1,9 @@
+var structdpp_1_1guild__scheduled__event__user__remove__t =
+[
+    [ "guild_scheduled_event_user_remove_t", "structdpp_1_1guild__scheduled__event__user__remove__t.html#afb7df9e7e9d1b6ff1d13496dceac8d3f", null ],
+    [ "cancel_event", "structdpp_1_1guild__scheduled__event__user__remove__t.html#a5afbcd36109734ad0fca14ec848b73b7", null ],
+    [ "is_cancelled", "structdpp_1_1guild__scheduled__event__user__remove__t.html#aca2880114f69b7ba62eb5137824c1432", null ],
+    [ "event", "structdpp_1_1guild__scheduled__event__user__remove__t.html#a666177abd9a95f51e02ded82576ebb83", null ],
+    [ "from", "structdpp_1_1guild__scheduled__event__user__remove__t.html#a357873be4dd30eef7928ae5f7c13e7db", null ],
+    [ "raw_event", "structdpp_1_1guild__scheduled__event__user__remove__t.html#a22df2fa7e547eaefbd65e501d8bf67b7", null ]
+];
