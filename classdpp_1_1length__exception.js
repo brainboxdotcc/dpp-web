bcc150120d22e044b9027b1a71727d2b9b8cf7c8
brain <craigedwards@brainbox.cc>
2021-11-17 10:48:05 +0000

    automatic commit

---
+++ classdpp_1_1length__exception.js
@@ -0,0 +1,5 @@
+var classdpp_1_1length__exception =
+[
+    [ "what", "classdpp_1_1length__exception.html#a010588e4eec074c113ff36c82dde74f9", null ],
+    [ "msg", "classdpp_1_1length__exception.html#ac033c933ff59daf22d4ea121303e11b9", null ]
+];
